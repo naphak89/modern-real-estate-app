ccd8b0d02dcea9914175b1da4d99d80e16af61f1
naphak89 <naphak.jaengjaikul@gmail.com>
2025-02-14 15:13:16 +0800

    flatlist update
    - scrollable screen
    - vertical and horizontal scroll on the property

--- realEstateApp/app/(root)/(tabs)/index.tsx
+++ realEstateApp/app/(root)/(tabs)/index.tsx
@@ -1,74 +1,82 @@
 import icons from "@/constants/icons";
 import images from "@/constants/images";
 import { Link } from "expo-router";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Search from "@/component/Search";
 import { Card, FeaturedCard } from "@/component/Cards";
 import Filters from "@/component/Filters";
+import { useGlobalContext } from "@/lib/global-provider";
 
 export default function Index() {
+  const { user } = useGlobalContext();
+
   return (
     <SafeAreaView className="bg-white h-full">
-      <View className="px-5 flex">
-        <View className="flex flex-row items-center justify-between mt-5">
-          <View className="flex flex-row items-center">
-            <Image
-              source={images.avatar}
-              className="size-12 rounded-full"
-            ></Image>
-            <View className="flex flex-col item-start ml-2 justify-center">
-              <Text className="text-xs font-rubik text-black-100">
-                Good Morning
-              </Text>
-              <Text className="text-base font-rubik-medium text-black-300">
-                Adrian
-              </Text>
+      <FlatList
+        data={[1, 2, 3, 4]}
+        renderItem={({ item }) => <Card />}
+        keyExtractor={(item) => item.toString()}
+        numColumns={2}
+        contentContainerClassName="pd-32"
+        columnWrapperClassName="flex gap-5 px-5"
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={
+          <View className="px-5 flex">
+            <View className="flex flex-row items-center justify-between mt-5">
+              <View className="flex flex-row items-center">
+                <Image
+                  source={{ uri: user?.avatar }}
+                  className="size-12 rounded-full"
+                ></Image>
+                <View className="flex flex-col item-start ml-2 justify-center">
+                  <Text className="text-xs font-rubik text-black-100">
+                    Good Morning
+                  </Text>
+                  <Text className="text-base font-rubik-medium text-black-300">
+                    {user?.name}
+                  </Text>
+                </View>
+              </View>
+
+              <Image source={icons.bell} className="size-6"></Image>
             </View>
-          </View>
-
-          <Image source={icons.bell} className="size-6"></Image>
-        </View>
-        <Search></Search>
-        <View className="my-5">
-          <View className="flex flex-row items-center justify-between">
-            <Text className="text-xl font-rubik-bold text-black-300">
-              Featured
-            </Text>
-            <TouchableOpacity>
-              <Text className="text-base font-rubik-bold text-primary-300">
-                See All
+            <Search></Search>
+            <View className="my-5">
+              <View className="flex flex-row items-center justify-between">
+                <Text className="text-xl font-rubik-bold text-black-300">
+                  Featured
+                </Text>
+                <TouchableOpacity>
+                  <Text className="text-base font-rubik-bold text-primary-300">
+                    See All
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              <FlatList
+                data={[1, 2, 3]}
+                renderItem={({ item }) => <FeaturedCard />}
+                keyExtractor={(item) => item.toString()}
+                horizontal
+                bounces={false}
+                showsHorizontalScrollIndicator={false}
+                contentContainerClassName="flex gap-5 mt-5"
+              ></FlatList>
+            </View>
+            <View className="flex flex-row items-center justify-between">
+              <Text className="text-xl font-rubik-bold text-black-300">
+                Our Recommendation
               </Text>
-            </TouchableOpacity>
+              <TouchableOpacity>
+                <Text className="text-base font-rubik-bold text-primary-300">
+                  See All
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <Filters />
           </View>
-
-          <View className="flex flex-row gap-5 mt-5">
-            <FeaturedCard />
-            <FeaturedCard />
-            <FeaturedCard />
-          </View>
-        </View>
-        <View className="flex flex-row items-center justify-between">
-          <Text className="text-xl font-rubik-bold text-black-300">
-            Our Recommendation
-          </Text>
-          <TouchableOpacity>
-            <Text className="text-base font-rubik-bold text-primary-300">
-              See All
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <Filters />
-
-        <View className="flex flex-row gap-5 mt-5">
-          <Card />
-          <Card />
-        </View>
-      </View>
+        }
+      ></FlatList>
     </SafeAreaView>
   );
 }
-/* <Link href="/signIn">Sign In</Link>
-      <Link href="/explore">explore</Link>
-      <Link href="/profile">profile</Link>
-      <Link href="/properties/1">Property</Link> */
--- realEstateApp/component/Filters.tsx
+++ realEstateApp/component/Filters.tsx
@@ -1,29 +1,24 @@
 import { View, Text, ScrollView, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
-import { useLocalSearchParams } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
 import { categories } from "@/constants/data";
-const FilterHandler = ({
-  item,
-  index,
-  onPress,
-}: {
-  item: string;
-  index: Number;
-  onPress?: () => void;
-}) => {
-  <TouchableOpacity>
-    <View className="bg-red-600 flex flex-row gap-3">
-      <Text>{item}</Text>
-    </View>
-  </TouchableOpacity>;
-};
 
 const Filters = () => {
   const param = useLocalSearchParams<{ filter?: string }>();
   const [selectedCategory, setSelectedCategory] = useState(
-    param.filter || "ALL"
+    param.filter || "All"
   );
-  const handleCategory = (category: string) => {};
+  const handleCategory = (category: string) => {
+    if (selectedCategory === category) {
+      setSelectedCategory("All");
+      router.setParams({ filter: "All" });
+      return;
+    }
+
+    setSelectedCategory(category);
+    router.setParams({ filter: category });
+    return;
+  };
   return (
     <ScrollView
       horizontal={true}
@@ -31,7 +26,25 @@ const Filters = () => {
       className="mt-3 mb-2"
     >
       {categories.map((item, index) => (
-        <handleCategory key={index} item={item.title} />
+        <TouchableOpacity
+          key={index}
+          onPress={() => handleCategory(item.category)}
+          className={`flex flex-col items-start mr-4 px-4 py-2 rounded-full ${
+            selectedCategory === item.category
+              ? "bg-primary-300"
+              : "bg-primary-100 border border-primary-200"
+          }`}
+        >
+          <Text
+            className={`text-sm ${
+              selectedCategory === item.category
+                ? "text-white font-rubik-bold mt-0.5"
+                : "text-black-300 font-rubik"
+            }`}
+          >
+            {item.category}
+          </Text>
+        </TouchableOpacity>
       ))}
     </ScrollView>
   );
